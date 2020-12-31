33b0f79c0452f33f70c9657b60db37dca5540ed4
C. L <amautacelt@gmail.com>
2020-12-31 03:36:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,6 +6,6 @@ def reverse_each_word(sentence)
     #phrase.reverse
   #end
   
-  sentence.split.collect {|word| word.reverse}.join("")
+  sentence.split.collect {|word| word.reverse}.join(" ")
   
 end
