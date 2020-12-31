1d99eb9f8c460e36d3fa81de7743ed4271fa17a2
C. L <amautacelt@gmail.com>
2020-12-31 03:11:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence)
   phrase = "Hello there, and how are you?"
   sentence.each do |sentence|
   puts "Hello #{sentence.collect}!"
+  end
 end
 
 #reverse_each_word("Hello there, and how are you?")
